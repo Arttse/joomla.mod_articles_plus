fec3cac4c4316f373d561296e2a0380e1dcd5e27
Nikita Bystrov <arttsesoft@gmail.com>
2018-05-22 19:30:00 +0300

    Update year to 2018

--- mod_articles_plus/mod_articles_plus.php
+++ mod_articles_plus/mod_articles_plus.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright      Copyright (C) 2016 Nikita «Arttse» Bystrov. All rights reserved.
+ * @copyright      Copyright (C) 2016-2018 Nikita «Arttse» Bystrov. All rights reserved.
  * @license        License GNU General Public License version 3
  * @author         Nikita «Arttse» Bystrov
  */
