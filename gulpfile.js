5d17da86082acbb28dafd292ab9c0a12e85ddce7
Nikita Bystrov <arttsesoft@gmail.com>
2018-05-22 20:17:46 +0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,15 @@
-var mod         = 'mod_articles_plus',
-    fs          = require ( 'fs' ),
-    manifestXML = fs.readFileSync ( './' + mod + '/' + mod + '.xml', 'utf8' ),
-    xml2json    = require ( 'xml2json' ),
-    manifest    = (JSON.parse ( xml2json.toJson ( manifestXML ) )).extension,
-    gulp        = require ( 'gulp' ),
-    zip         = require ( 'gulp-zip' );
+const fs = require('fs')
+const xml2json = require('xml2json')
+const gulp = require('gulp')
+const zip = require('gulp-zip')
 
-gulp.task ( 'create.installer', function () {
-    return gulp
-        .src ( './' + mod + '/**' )
-        .pipe ( zip ( mod + '__' + manifest.version[1] + '__installer.zip' ) )
-        .pipe ( gulp.dest ( './.installers' ) );
-} );
+const modName = 'mod_articles_plus'
+const manifestXML = fs.readFileSync('./' + modName + '/' + modName + '.xml', 'utf8')
+const manifest = (JSON.parse(xml2json.toJson(manifestXML))).extension
 
-gulp.task ( 'tests', function () {
-    console.log( 'Sorry! No tests.' );
-} );
+gulp.task('create.installer', () =>
+  gulp
+    .src('./' + modName + '/**')
+    .pipe(zip(modName + '__' + manifest.version[1] + '__installer.zip'))
+    .pipe(gulp.dest('./.installers'))
+)
