ec677488e93c60d2a3fd3de59e6c3041080ded3a
Nikita Bystrov <arttsesoft@gmail.com>
2018-05-22 20:58:58 +0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const gulp = require('gulp')
 const zip = require('gulp-zip')
 
 const modName = 'mod_articles_plus'
-const manifestXML = fs.readFileSync('./' + modName + '/' + modName + '.xml', 'utf8')
+const manifestXML = fs.readFileSync(`./${modName}/${modName}.xml`, 'utf8')
 const manifest = (JSON.parse(xml2json.toJson(manifestXML))).extension
 
 gulp.task('create.installer', () =>
   gulp
-    .src('./' + modName + '/**')
-    .pipe(zip(modName + '__' + manifest.version[1] + '__installer.zip'))
+    .src(`./${modName}/**`)
+    .pipe(zip(`${modName}__${manifest.version[1]}__installer.zip`))
     .pipe(gulp.dest('./.installers'))
 )
